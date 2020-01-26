aedbaf36be1bf3123b06781c1bd4afa145a474d4
darryl <darryl.sulistyan@gmail.com>
2020-01-26 12:34:12 +0700

    answer 1-7

---
+++ machinelearningbasic/learningfromdata/hw5.py
@@ -0,0 +1,56 @@
+import os
+import re
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Q1():
+    s = 0.1
+    d = 8
+    for E in [0.007, 0.008, 0.009]:
+        N = s**2 * (d+1) / (s**2 - E)
+        print("Q1: E: {}, N: {}".format(E, N))
+
+def Q4_5_6():
+    # E(u,v) = (ue^v - 2ve^-u)^2
+    # dE/du = 2 * (ue^v - 2ve^-u) * (e^v + 2ve^-u)
+    # dE/dv = 2 * (ue^v - 2ve^-u) * (ue^v - 2e^-u)
+    
+    print("Q4: 2 * (ue^v - 2ve^-u) * (e^v + 2ve^-u)")
+
+    u,v = (1,1)
+    lr = 0.1
+    I = 0
+    while True:
+        du = 2 * (u*math.exp(v) - 2*v*math.exp(-u)) * (math.exp(v) + 2*v*math.exp(-u))
+        dv = 2 * (u*math.exp(v) - 2*v*math.exp(-u)) * (u*math.exp(v) - 2*math.exp(-u))
+        u,v = (u - lr*du, v - lr*dv)
+        Euv = (u*math.exp(v) - 2*v*math.exp(-u))**2
+        if Euv < 1e-14:
+            break
+        I += 1
+    
+    print("Q5: I = {}\nQ6: (u,v)=({}, {})".format(I, u, v))
+
+def Q7():
+    u,v = (1,1)
+    lr = 0.1
+    I = 0
+    totalIterations = 1000
+    while I < totalIterations:
+        du = 2 * (u*math.exp(v) - 2*v*math.exp(-u)) * (math.exp(v) + 2*v*math.exp(-u))
+        u,v = (u - lr*du, v)
+
+        dv = 2 * (u*math.exp(v) - 2*v*math.exp(-u)) * (u*math.exp(v) - 2*math.exp(-u))
+        u,v = (u, v - lr*dv)
+        I += 1
+    
+    Euv = (u*math.exp(v) - 2*v*math.exp(-u))**2
+    print("Q7: ({},{}) => E(u,v):{}".format(u, v, Euv))
+
+def main():
+    Q7()
+
+if __name__ == '__main__':
+    main()
