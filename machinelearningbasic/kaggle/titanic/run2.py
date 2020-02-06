440bac60e562abb6af55667c494dabd80959b6e9
ThinkPad L460 <darryl.sulistyan@global-infotech.co.id>
2020-02-06 17:29:42 +0700

    attempt 2 at titanic

---
+++ machinelearningbasic/kaggle/titanic/run2.py
@@ -0,0 +1,288 @@
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import math
+
+from sklearn.model_selection import KFold
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import roc_auc_score
+
+def getTitleCodeFromName(strName):
+    titleMap = {
+        "mr": 1,
+        "miss": 2,
+        "mrs": 3,
+        "master": 4,
+        "rev": 5,
+        "col": 6,
+        "dona": 7,
+        "dr": 5,
+        "ms": 3,
+        "mlle": 2,
+        "major": 6,
+        "don": 5,
+        "mme": 7,
+        "capt": 6,
+        "jonkheer": 5,
+        "sir": 5,
+        "the countess": 7,
+        "lady": 7
+    }
+
+    return titleMap[getTitleFromName(strName)]
+
+def getTitleFromName(strName):
+    givenName = strName.split(",")[1]
+    title = givenName.split(".")[0].lower().strip()
+    return title
+
+def showTitleVsSurvival(data):
+    data["Title"] = data["Name"].apply(getTitleFromName)
+    sns.countplot(x="Title", hue="Survived", data=data)
+    plt.legend()
+    plt.show()
+
+def showAgeVsSurvival(data):
+    """
+    More females survive, most survival is in age ~20-40 or so.
+    """
+    sns.distplot(data[data["Survived"] == 1].Age.dropna(), bins=20, kde=False, label="survived")
+    sns.distplot(data[data["Survived"] == 0].Age.dropna(), bins=20, kde=False, label="dead")
+    plt.legend()
+    plt.show()
+
+def showAgeGenderVsSurvival(data):
+    """
+    Majority male not survived, majority female survived. Majority young kids, both M/F survived.
+    """
+    sns.swarmplot(data=data, x="Sex", y="Age", hue="Survived")
+    plt.legend()
+    plt.show()
+
+def showFarePClassVsSurvived(data):
+    """
+    Majority Pclass 1 survived, and its fare is the highest. Others majority are not survived.
+    """
+    sns.swarmplot(data=data, x="Pclass", y="Fare", hue="Survived")
+    plt.legend()
+    plt.show()
+
+def showEmbarkedFareVsSurvived(data):
+    """
+    Not very clear on relationship
+    """
+    sns.swarmplot(data=data, x="Embarked", y="Fare", hue="Survived")
+    plt.legend()
+    plt.show()
+
+def showSiblingSpouseVsSurvived(data):
+    """
+    Fascinating, survival is most likely for SibSp <= 3 and Parch <= 3
+    This could be investigated further. Is this correlated to Pclass?
+    """
+    sns.violinplot(data=data, x="SibSp", y="Parch", hue="Survived")
+    plt.legend()
+    plt.show()
+
+def showFamilySizeVsSurvived(data):
+    data["FamilySize"] = data["SibSp"] + data["Parch"]
+    sns.countplot(x="FamilySize", hue="Survived",  data=data)
+    plt.legend()
+    plt.show()
+
+def showSibSpParchPclass(data):
+    """
+    As expected, the higher SibSp and Parch are in lower Pclass and they don't survive, 
+    supported the conclusion from Pclass vs survivability earlier.
+    """
+    sns.scatterplot(data=data, x="SibSp", y="Parch", hue="Pclass")
+    plt.legend()
+    plt.show()
+
+def showCabinVsSurvived(data):
+    """
+    Seems promising for B,C,D and E cabins, but not much elsewhere.
+    The problem for cabin is that ~70% of the data is MISSING!
+    For missing data, majority of them not survived.
+    """
+    data2 = data[["Cabin", "Survived"]].fillna('X')
+    data2["CabinFL"] = data2["Cabin"].apply(lambda x: x[0])
+    sns.countplot(x="CabinFL", hue="Survived", data=data2)
+    plt.legend()
+    plt.show()
+
+def showCabinVsFare(data):
+    """
+    Cabin BCDE seems to be the most expensive.
+    Cabin AFTG should be combined to one label: Other.
+    """
+    data2 = data[["Cabin", "Fare", "Survived"]].dropna()
+    data2["CabinFL"] = data2["Cabin"].apply(lambda x: x[0])
+    sns.swarmplot(data=data2, x="CabinFL", y="Fare")
+    plt.legend()
+    plt.show()
+
+def showFareDist(data):
+    data["FareLog"] = data["Fare"].apply(lambda x : math.log(x+1))
+
+    plt.subplot(121)
+    plt.tight_layout()
+    sns.violinplot(data=data, x="Survived", y="Fare")
+    plt.title("Survived vs Fare")
+
+    plt.subplot(122)
+    plt.tight_layout()
+    sns.violinplot(data=data, x="Survived", y="FareLog")
+    plt.title("Survived vs FareLog")
+
+    plt.legend()
+    plt.show()
+
+def rfattempt1_transform(X_train, X_test):
+    # encode Sex
+    sexEncoder = LabelEncoder()
+    sexEncoder = sexEncoder.fit(X_train["Sex"])
+    X_train.loc[:,["Sex"]] = sexEncoder.transform(X_train["Sex"])
+    X_test.loc[:,["Sex"]] = sexEncoder.transform(X_test["Sex"])
+
+    # fill age with median
+    medianAge = np.median(X_train["Age"].dropna())
+    X_train.loc[:,["Age"]] = X_train["Age"].fillna(medianAge)
+    X_test.loc[:,["Age"]] = X_test["Age"].fillna(medianAge)
+
+    # MaxFam lowers the score.
+    # create new feature, maxfam 
+    #def mxs(row):
+    #    return max(row.SibSp, row.Parch)
+    #X_train["MaxFam"] = X_train.apply(mxs, axis=1)
+    #X_test["MaxFam"] = X_test.apply(mxs, axis=1)
+
+    # Fare
+    medianFare = np.median(X_train["Fare"].dropna())
+    X_train.loc[:,["Fare"]] = X_train["Fare"].fillna(medianFare)
+    X_test.loc[:,["Fare"]] = X_test["Fare"].fillna(medianFare)
+
+    # Cabin
+    # Cabin reduces score. Since it's missing ~70%, just remove this.
+    #cabinCode = {
+    #    "A": 1,
+    #    "B": 2,
+    #    "C": 3,
+    #    "D": 4,
+    #    "E": 5,
+    #    "F": 6,
+    #    "G": 7,
+    #    "T": 8,
+    #    "U": 9
+    #}
+    #X_train.loc[:,["Cabin"]] = X_train["Cabin"].fillna("U")
+    #X_train.loc[:,["Cabin"]] = X_train["Cabin"].apply(lambda x: cabinCode[x[0]])
+    
+    #X_test.loc[:,["Cabin"]] = X_test["Cabin"].fillna("U")
+    #X_test.loc[:,["Cabin"]] = X_test["Cabin"].apply(lambda x: cabinCode[x[0]])
+
+    X_train = X_train.drop(["Cabin"], axis=1)
+    X_test = X_test.drop(["Cabin"], axis=1)
+
+    # embarked
+    X_train.loc[:,["Embarked"]] = X_train["Embarked"].fillna("S")
+    embEncoder = LabelEncoder()
+    embEncoder = embEncoder.fit(X_train["Embarked"])
+    X_train.loc[:,["Embarked"]] = embEncoder.transform(X_train["Embarked"])
+    X_test.loc[:,["Embarked"]] = embEncoder.transform(X_test["Embarked"].fillna("S"))
+
+    # add a little bit (0.76)
+    X_train["Title"] = X_train["Name"].apply(getTitleCodeFromName)
+    X_test["Title"] = X_test["Name"].apply(getTitleCodeFromName)
+
+    X_train = X_train.drop(["Name"], axis=1)
+    X_test = X_test.drop(["Name"], axis=1)
+
+    return X_train, X_test
+
+def rfattempt1(df_train, df_forSubs):
+    kf = KFold(n_splits=5)
+
+    oriFeatures = df_train.columns.drop(["PassengerId","Survived","Ticket"])
+    X = df_train[oriFeatures]
+    y = df_train["Survived"]
+
+    aucList = []
+
+    for train_idx, test_idx in kf.split(X):
+        X_train, X_test = X.iloc[train_idx,:], X.iloc[test_idx,:]
+        y_train, y_test = y[train_idx], y[test_idx]
+
+        X_train, X_test = rfattempt1_transform(X_train, X_test)
+
+        model = RandomForestClassifier()
+        model = model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        importances = pd.DataFrame({'feature':X_train.columns,'importance':np.round(model.feature_importances_,3)})
+        importances = importances.sort_values('importance',ascending=False).set_index('feature')
+        print(importances)
+
+        auc = roc_auc_score(y_test, y_pred)
+        aucList.append(auc)
+        
+    print("Avg auc: {}".format(np.mean(aucList)))
+
+    print("Predicting result in test.csv")
+    X_p = df_forSubs[oriFeatures]
+
+    X_train, X_test = rfattempt1_transform(X, X_p)
+    #print(X_train.describe(include='all'))
+    #print(X_test.describe(include='all'))
+
+    model = RandomForestClassifier()
+    model = model.fit(X_train, y)
+    y_p = model.predict(X_test)
+
+    df_forSubs["Survived"] = y_p
+    df_forSubs[["PassengerId", "Survived"]].to_csv("test.predicted.csv", index=False)
+
+    print("Done")
+
+def showTitles(x1, x2):
+    x1title = x1["Name"].apply(getTitleFromName)
+    x2title = x2["Name"].apply(getTitleFromName)
+    print(x1title.value_counts())
+    print(x2title.value_counts())
+    #print(pd.Series(titles).value_counts())
+
+
+def main():
+    df_train = pd.read_csv('train.csv')
+    df_test = pd.read_csv('test.csv')
+
+    oriFeatures = df_train.columns.drop(["PassengerId","Survived"])
+
+    #print("CabinFL: {}".format(df_train["Cabin"].dropna().apply(lambda x: x[0]).value_counts()))
+    #print(df_train[oriFeatures].describe(include='all'))
+    #print(df_train["Embarked"].value_counts())
+    #print(df_train["Cabin"].value_counts())
+    #print(df_train["Pclass"].value_counts())
+    #showAgeVsSurvival(df_train)
+    #showAgeGenderVsSurvival(df_train)
+    #showFareVsPClass(df_train)
+    #showEmbarkedFareVsSurvived(df_train)
+    #showSiblingSpouseVsSurvived(df_train)
+    showFamilySizeVsSurvived(df_train)
+    #showSibSpParchPclass(df_train)
+    #print("Unique cabin numbers: {}".format(np.unique(df_train["Cabin"].dropna())))
+    #showCabinVsSurvived(df_train)
+    #showCabinVsFare(df_train)
+    #showTitleVsSurvival(df_train)
+    #showFareDist(df_train)
+    #rfattempt1(df_train, df_test)
+    #showTitles(df_train, df_test)
+
+if __name__ == '__main__':
+    main()
